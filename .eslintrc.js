f08c613520227f14a1c38feb486478d464a8fc0d
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2022-04-24 12:49:43 +0200

    Run vue add eslint to create a dedicated confi file

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,14 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    '@vue/standard'
   ],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
-    "no-unused-vars": process.env.NODE_ENV === 'production' ? 'error' : 'warn'
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-};
+}
