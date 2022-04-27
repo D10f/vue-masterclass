e30458738e5925d3b60b9e78622bbca80a154bd1
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2022-04-27 20:44:50 +0200

    Installed vue-router4

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
 <template>
-  <img src="./assets/logo.png" alt="Vue logo" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <img src="./assets/logo.png" alt="Vue logo" />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data() {
-    return {
-      test: 123,
-    };
-  },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
